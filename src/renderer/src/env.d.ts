308fa76ed40ca2dd9dcd5ac395e53d5cf401bce9
David Northover <david.northover@psu.edu>
2026-07-02 23:49:10 -0400

    Bound file explorer watcher

--- src/renderer/src/env.d.ts
+++ src/renderer/src/env.d.ts
@@ -1,1 +1,15 @@
 /// <reference types="vite/client" />
+
+import type { JSX as ReactJSX } from 'react'
+
+declare global {
+	namespace JSX {
+		type Element = ReactJSX.Element
+		type ElementType = ReactJSX.ElementType
+		interface ElementClass extends ReactJSX.ElementClass {}
+		interface ElementAttributesProperty extends ReactJSX.ElementAttributesProperty {}
+		interface ElementChildrenAttribute extends ReactJSX.ElementChildrenAttribute {}
+		interface IntrinsicAttributes extends ReactJSX.IntrinsicAttributes {}
+		interface IntrinsicElements extends ReactJSX.IntrinsicElements {}
+	}
+}
